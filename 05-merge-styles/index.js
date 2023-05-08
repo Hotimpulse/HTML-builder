bb256279fdb6d9ffc14ce31e19acb3972bb1e1a8
Lev <hotimpulse@hotmail.com>
2023-05-08 21:36:14 +0700

    feat: adding the 5th task

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,39 @@
+import fs from "fs";
+import path from "path";
+
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+const stylesFolder = path.join(__dirname, "styles");
+const distFolder = path.join(__dirname, "project-dist");
+
+const getStyles = async () => {
+    const files = await fs.promises.readdir(stylesFolder);
+
+    const cssFiles = files.filter(file => {
+        const extension = path.extname(file);
+        return extension === ".css";
+    })
+
+    const cssStyles = await Promise.all(cssFiles.map(async (file) => {
+        const stylePath = path.join(stylesFolder, file);
+        const contents = await fs.promises.readFile(stylePath, "utf-8");
+        return contents;
+    }))
+
+    const cssBundle = cssStyles.join("\n");
+    return cssBundle;
+}
+
+const writeToBundle = async cssBundle => {
+    const bundlePath = path.join(distFolder, "bundle.css");
+    await fs.promises.writeFile(bundlePath, cssBundle);
+}
+
+(async () => {
+    try {
+        const bundle = await getStyles();
+        await writeToBundle(bundle);
+        console.log("Bundle created!")
+    } catch (err) {
+        console.error(err);
+    }
+})();
